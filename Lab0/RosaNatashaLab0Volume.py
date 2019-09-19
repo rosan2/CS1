31fe9cba45080f8492462998f81ae25408be72b3
unknown <rosan2@rpi.edu>
2019-09-19 14:17:53 -0400

    added labs

---
+++ Lab0/RosaNatashaLab0Volume.py
@@ -0,0 +1,9 @@
+length = 16.5
+width = 12.5
+height = 5
+
+volume = length * height * width
+sArea = 2 * (length * height) + 2 * (height * width) + 2 * (length * width)
+
+print("volume =", volume)
+print("area =", sArea)
